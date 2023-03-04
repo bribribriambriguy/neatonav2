a705c36261a16aacdb48f59fdac2609b80d754a2
bribribriambriguy <bkletchikov@yahoo.com>
2023-03-04 10:20:08 -0800

    added urdf files to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share',package_name, 'launch'), glob('launch/*launch.py')),
+        (os.path.join('share',package_name, 'urdf'), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
