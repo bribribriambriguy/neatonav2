2f67dd03cb4a0dfe085ca3ad9708af2ac00950aa
bribribriambriguy <bkletchikov@gmail.com>
2023-03-04 10:31:19 -0800

    added config files to setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         (os.path.join('share',package_name, 'launch'), glob('launch/*launch.py')),
         (os.path.join('share',package_name, 'urdf'), glob('urdf/*')),
         (os.path.join('share',package_name, 'map'), glob('map/*')),
+        (os.path.join('share',package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
