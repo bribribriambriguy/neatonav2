4e0e49ef8491a69d5bb66ab3ff0358dc111c34fa
bribribriambriguy <bkletchikov@yahoo.com>
2023-03-11 19:51:26 -0800

    changed setup.py to only allow .py in launch

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share',package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share',package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share',package_name, 'urdf'), glob('urdf/*')),
         (os.path.join('share',package_name, 'map'), glob('map/*')),
         (os.path.join('share',package_name, 'config'), glob('config/*')),
